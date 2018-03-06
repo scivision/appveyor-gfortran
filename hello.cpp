51c5ed34e7878e266ed77a36d48f9070458add01
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-06 18:10:30 -0500

    use

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#define cxx_ver __VERSION__
+
 int main() 
 {
     std::cout << "Hello, World! (from C++)" << std::endl;
+    std::cout << cxx_ver << std::endl;
+    
     return EXIT_SUCCESS;
 }
--- hello.f90
+++ hello.f90
@@ -1,5 +1,7 @@
 program hello
 
+use, intrinsic:: iso_fortran_env
+
 print *,'hello world! (from Fortran)'
 print *,compiler_version()
 print *,compiler_options()
